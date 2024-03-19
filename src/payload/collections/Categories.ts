7629bfe65435a567ab52208d2a6bb8709ee7131d
kalinchkma <chakma.kalin10211@gmail.com>
2024-03-20 01:46:42 +0600

    Initialized project

---
+++ src/payload/collections/Categories.ts
@@ -0,0 +1,23 @@
+import type { CollectionConfig } from 'payload/types'
+
+const Categories: CollectionConfig = {
+  slug: 'categories',
+  admin: {
+    useAsTitle: 'title',
+  },
+  access: {
+    read: () => true,
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+    },
+    {
+      name: 'test',
+      type: 'text',
+    },
+  ],
+}
+
+export default Categories
